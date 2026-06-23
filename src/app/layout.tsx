9ab1e5309355c8f1d3a5ac2b0ee314999ebbf4f6
lsmxy4 <lsmxy4@gmail.com>
2026-06-23 13:44:46 +0900

    reactQuery

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
+import Providers from './providers';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -47,8 +48,11 @@ export default function RootLayout({
 				</header>
 
 				<main className="mx-auto min-h-[calc(100vh-112px)] w-[1190px] py-8">
-					{children}
+					<Providers>
+						{children}
+					</Providers>
 				</main>
+
 
 				<footer className="border-t bg-white">
 					<div className="mx-auto flex h-14 w-[1190px] items-center text-[12px] text-gray-500">
